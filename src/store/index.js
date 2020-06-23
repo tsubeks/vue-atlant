c610f0a62a9b305ef96231a01abab0ce14943199
Bogdan Tsubeks <tsubeks@gmail.com>
2020-06-23 19:50:38 +0300

    persist store in localStorage for desk moduel

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
+
 import transactions from "@/store/modules/transactions";
 import desk from "@/store/modules/desk";
 
 Vue.use(Vuex);
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  modules: ["desk"], //only save user module
+});
 
 export default new Vuex.Store({
   modules: {
     transactions,
     desk,
   },
+  plugins: [vuexLocal.plugin],
 });
