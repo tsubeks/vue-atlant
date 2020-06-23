ef5c27edf295d9728cc76479609f92c66f5aba66
Bogdan Tsubeks <tsubeks@gmail.com>
2020-06-23 19:40:27 +0300

    comment desk module in store

--- src/store/modules/desk.js
+++ src/store/modules/desk.js
@@ -1,4 +1,3 @@
-// initial state
 const state = () => ({
   inc: 10,
   minSize: 100,
@@ -12,68 +11,82 @@ const state = () => ({
   removed: [{ id: "4", x: 180, y: 300, width: 300, height: 200 }],
 });
 
-// getters
 const getters = {};
 
-// actions
 const actions = {};
 
-// mutations
+// FIXME: there is no common util for flexibility
 const mutations = {
   onResize: function(state, { id, x, y, width, height }) {
+    // find index in array by id
     const idx = state.blocks.findIndex((block) => block.id === id);
 
+    // right order
     let indexOfTopOrder = state.order.indexOf(state.order.length - 1);
     state.order[indexOfTopOrder] = state.order[id];
     state.order[id] = state.order.length - 1;
 
+    // update block
     state.blocks[idx] = { id: state.blocks[idx].id, x, y, width, height };
+
+    // update links
     state.blocks = [...state.blocks];
     state.order = [...state.order];
   },
   onDrag: function(state, { id, x, y }) {
+    // find index in array by id
     const idx = state.blocks.findIndex((block) => block.id === id);
 
+    // right order
     let indexOfTopOrder = state.order.indexOf(state.order.length - 1);
     state.order[indexOfTopOrder] = state.order[id];
     state.order[id] = state.order.length - 1;
 
+    // update block
     state.blocks[idx] = { ...state.blocks[idx], x, y };
+
+    // update links
     state.blocks = [...state.blocks];
     state.order = [...state.order];
   },
   onActivated: function(state, { id }) {
+    // right order
     let indexOfTopOrder = state.order.indexOf(state.order.length - 1);
     state.order[indexOfTopOrder] = state.order[id];
     state.order[id] = state.order.length - 1;
+
+    // update links
     state.order = [...state.order];
     state.blocks = [...state.blocks];
   },
   onRemove: function(state, { id }) {
+    // find index in array by id
     const idx = state.blocks.findIndex((block) => block.id === id);
-    console.log(idx);
 
-    // TODO: order?
+    // TODO: check if the order is right or not here
+    // update values and links
     state.removed = [...state.removed, state.blocks[idx]];
     state.blocks = [...state.blocks.filter((block) => block.id !== id)];
-    console.log(state.blocks);
   },
   onRestore: function(state, { x, y }) {
-    // restore last
+    let defaultHeight = 100;
+    let defaultWidth = 300;
+    // restore last, it can be any id in the future
     const id = state.removed[state.removed.length - 1].id;
     const idx = state.removed.findIndex((block) => block.id === id);
-    // TODO: order?
+
+    // TODO: check if the order is right or not here
+    // update values and links
     state.blocks = [
       ...state.blocks,
       {
         ...state.removed[idx],
-        x: x - 150,
-        y: y - 50,
-        width: 300,
-        height: 100,
+        x: x - defaultWidth / 2,
+        y: y - defaultHeight / 2,
+        width: defaultWidth,
+        height: defaultHeight,
       },
     ];
-
     state.removed = [...state.removed.filter((block) => block.id !== id)];
   },
 };
