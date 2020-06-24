f970997b4fb157ae250ccb142c50f25e60c96dd5
Bogdan Tsubeks <tsubeks@gmail.com>
2020-06-24 09:35:32 +0300

    update state persist plugin to vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import VuexPersistence from "vuex-persist";
+import createPersistedState from "vuex-persistedstate";
 
 import transactions from "@/store/modules/transactions";
 import desk from "@/store/modules/desk";
 
 Vue.use(Vuex);
 
-const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
-  modules: ["desk"], //only save user module
-});
+// TODO: save only desk or everything?
+// const deskState = new createPersistedState({
+//   paths: ["desk"],
+// });
 
 export default new Vuex.Store({
   modules: {
     transactions,
     desk,
   },
-  plugins: [vuexLocal.plugin],
+  plugins: [createPersistedState()],
 });
